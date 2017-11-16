9fea47b2e7933b79e1794e32fbe7ae798545a3bd
nigelflippo <flippo.nigel@gmail.com>
2017-11-15 22:08:36 -0700

    Added revised exercises

--- main.js
+++ main.js
@@ -1,37 +1,32 @@
 //use rest in function parameter
 //if input is (1, 2, 3), the function should return [2, 4, 6]
-const rest = (...input) => input.map(element => element * 2)
+//the function should account for n number of arguments
+const rest = input => input.map(element => element * 2);
 
-function rest(input) {
-  return input.map(function(element) {
-    return element * 2;
-  });
-};
-
-//use spread in function call
-//if input is [1, 2, 3], spread will return 6
-const add = (...numbers) => numbers.reduce((sum, element) => sum + element)
-const spread = input => add(...input)
-
-function add(...numbers) {
-  return numbers.reduce(function(sum, element) {
-    return sum + element;
-  });
-};
-function spread(input) {
-  return add(input);
-};
+// function rest(input) {
+//   return input.map(function(element) {
+//     return element * 2;
+//   });
+// };
 
 //use spread to concat arrays
 //expect ([1, 2, 3], [4, 5, 6]) to equal [1, 2, 3, 4, 5, 6]
-const concat = (first, last) => [...first, ...last]
+const spread = (first, last) => first.concat(last);
 
-function concat(first, last) {
-  return first.concat(last);
-};
+// function concat(first, last) {
+//   return first.concat(last);
+// };
+
+//use spread with Math.max/min
+//if input is [1, 2, 4, 7, 9], return 9
+const mathSpread = numbers => Math.max(numbers);
+
+// function mathSpread(numbers) {
+//   return Math.max(numbers);
+// };
 
 module.exports = {
   rest,
   spread,
-  concat
+  mathSpread
 }
--- test/main.test.js
+++ test/main.test.js
@@ -6,6 +6,9 @@ describe('rest', function() {
   it('should return the correct array', function() {
     expect(main.rest(1, 2, 3)).to.deep.equal([2, 4, 6])
   })
+  it('should account for n number of arguments', function() {
+    expect(main.rest(1, 2, 3, 4, 5, 6)).to.deep.equal([2, 4, 6, 8, 10, 12])
+  })
   it('should use the rest operator', function() {
     const match = main.rest.toString().includes('...')
     expect(match).to.equal(true)
@@ -13,8 +16,8 @@ describe('rest', function() {
 })
 
 describe('spread', function() {
-  it('should return the correct result', function() {
-    expect(main.spread([1, 2, 3])).to.equal(6)
+  it('should concat two arrays', function() {
+    expect(main.spread([1, 2, 3], [4, 5, 6])).to.deep.equal([1, 2, 3, 4, 5, 6])
   })
   it('should use the spread operator', function() {
     const match = main.spread.toString().includes('...')
@@ -22,12 +25,12 @@ describe('spread', function() {
   })
 })
 
-describe('concat', function() {
-  it('should concat two arrays', function() {
-    expect(main.concat([1, 2, 3], [4, 5, 6])).to.deep.equal([1, 2, 3, 4, 5, 6])
+describe('math spread', function() {
+  it('should return the max number in array', function() {
+    expect(main.mathSpread([1, 2, 4, 7, 9])).to.equal(9)
   })
   it('should use the spread operator', function() {
-    const match = main.concat.toString().includes('...')
+    const match = main.mathSpread.toString().includes('...')
     expect(match).to.equal(true)
   })
 })
