c299dd5677b21f3299a69bf1cdd79d78bc60cd9f
nigelflippo <flippo.nigel@gmail.com>
2017-11-16 08:33:39 -0700

    Clarified objectives

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+//modify the functions below to satisfy the tests
+
 //use rest in function parameter
-//if input is (1, 2, 3), the function should return [2, 4, 6]
+//if input is (1, 2, 3) the function should return [2, 4, 6]
 //the function should account for n number of arguments
 const rest = input => input.map(element => element * 2);
 
@@ -10,7 +12,7 @@ const rest = input => input.map(element => element * 2);
 // };
 
 //use spread to concat arrays
-//expect ([1, 2, 3], [4, 5, 6]) to equal [1, 2, 3, 4, 5, 6]
+//if first and last are ([1, 2, 3], [4, 5, 6]) return [1, 2, 3, 4, 5, 6]
 const spread = (first, last) => first.concat(last);
 
 // function concat(first, last) {
@@ -18,11 +20,13 @@ const spread = (first, last) => first.concat(last);
 // };
 
 //use spread with Math.max/min
-//if input is [1, 2, 4, 7, 9], return 9
+//if input is [1, 2, 4, 7, 9] return 9
 const mathSpread = numbers => Math.max(numbers);
 
+//circumvents the use of apply
+
 // function mathSpread(numbers) {
-//   return Math.max(numbers);
+//   return Math.max.apply(null, numbers);
 // };
 
 module.exports = {
